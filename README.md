e74a4b651be3592f36d06ded3f9990d5f7415973
陈天华 <845758376@qq.com>
2025-10-27 11:32:02 +0800

    更新

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # 845758376.github.io
+# cth个人博客
+
+
+使用pnpm 安装的vuepress-theme-hope 主题
+
+https://845758376.github.io/
+
+vue-press-theme-hope 官方文档
+https://vuepress-theme-hope.github.io/v2/zh/guide/intro/install.html#doc-changelog
---
+++ src/.vuepress/config.ts
@@ -0,0 +1,16 @@
+import { defineUserConfig } from "vuepress";
+
+import theme from "./theme.js";
+
+export default defineUserConfig({
+  base: "/",
+
+  lang: "zh-CN",
+  title: "cth个人博客",
+  description: "cth个人博客",
+
+  theme,
+
+  // 和 PWA 一起启用
+  // shouldPrefetch: false,
+});
